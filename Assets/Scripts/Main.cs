f67c64cc97baf713541b0bdf65770816f2531a6c
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-12 16:35:26 +0900

    Mainクラス修正

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+	private void Start()
+	{
+		StartGame();
+	}
+
+	public void StartGame()
+	{
+		new Ness().Talk();
+		new Jeff().Talk();
+		new Paula().Talk();
+		new Poo().Talk();
+		new MasterBelch().Talk();
+		new Starman().Talk();
+		new Mama().Talk();
+		new MrSaturn().Talk();
+	}
+}
