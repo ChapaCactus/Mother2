7bb1f4a20cdd202be7c213927d9cb7418e9df17e
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-12 17:53:15 +0900

    各キャラクタークラス削除

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -11,28 +11,28 @@ public class Main : MonoBehaviour
 
 	public void StartGame()
 	{
-		var ness = new Ness();
+		var ness = new Player();
 		ness.Name = "ネス";
 		ness.TalkMessage = "...";
-		var paula = new Paula();
+		var paula = new Player();
 		paula.Name = "ポーラ";
 		paula.TalkMessage = "…わたしもちょっとくらいならアブナイ超能力を使えるのよ。";
-		var jeff = new Jeff();
+		var jeff = new Player();
 		jeff.Name = "ジェフ";
 		jeff.TalkMessage = "説明はいらないよ。ぼくはジェフ。きみたちに呼ばれて来たんだ。";
-		var poo = new Poo();
+		var poo = new Player();
 		poo.Name = "プー";
 		poo.TalkMessage = "おれの名はプー。君達と共に戦う者だ。おれはネスに従う。";
-		var starman = new Starman();
+		var starman = new Enemy();
 		starman.Name = "スターマン";
 		starman.TalkMessage = "オマエハ 英雄デハナク タダノムシケラナノダ！";
-		var masterBelch = new MasterBelch();
+		var masterBelch = new Enemy();
 		masterBelch.Name = "ゲップー";
 		masterBelch.TalkMessage = "ゲボゲボゲボ、ゲボゲボ、ゲローーップ！";
-		var mama = new Mama();
+		var mama = new NPC();
 		mama.Name = "ママ";
 		mama.TalkMessage = "イエーイ！ファイト！ファ・イ・ト♪";
-		var mrSaturn = new MrSaturn();
+		var mrSaturn = new NPC();
 		mrSaturn.Name = "どせいさん";
 		mrSaturn.TalkMessage = "ぽえ〜ん";
 	}
