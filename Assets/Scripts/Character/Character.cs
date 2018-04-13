7ad031b3afc998b8b8e6eff0b5ec8c055e8343cd
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-13 14:52:22 +0900

    HP設定

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -7,18 +7,21 @@ public abstract class Character : MonoBehaviour
 {
 	public string Name { get; private set; }
 	public string TalkMessage { get; private set; }
+
 	public int Power { get; private set; }
+	public int HP { get; private set; }
 
 	public virtual void Talk()
 	{
 		Debug.Log(Name + " 「" + TalkMessage + "」");
 	}
 
-	public void SetData(string name, string talkMessage, int power)
+	public void SetData(string name, string talkMessage, int power, int hp)
 	{
 		Name = name;
 		TalkMessage = talkMessage;
 		Power = power;
+		HP = hp;
 	}
 
 	protected abstract void OnTriggerEnter2D(Collider2D collision);
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -11,24 +11,24 @@ public class Main : MonoBehaviour
 		StartGame();
 	}
 
-	private Character CreateCharacter(string prefabPath, Transform parent, string name, string talkMessage, int power)
+	private Character CreateCharacter(string prefabPath, Transform parent, string name, string talkMessage, int power, int hp)
 	{
 		var prefab = Resources.Load(prefabPath) as GameObject;
 		var character = Instantiate(prefab, parent).GetComponent<Character>();
-		character.SetData(name, talkMessage, power);
+		character.SetData(name, talkMessage, power, hp);
 
 		return character;
 	}
 
 	public void StartGame()
 	{
-		var ness = CreateCharacter(Player.PrefabPath, transform, "ネス", "...", 100) as Player;
-		var paula = CreateCharacter(Player.PrefabPath, transform, "ポーラ", "…わたしもちょっとくらいならアブナイ超能力を使えるのよ。", 100) as Player;
-		var jeff = CreateCharacter(Player.PrefabPath, transform, "ジェフ", "説明はいらないよ。ぼくはジェフ。きみたちに呼ばれて来たんだ。", 100) as Player;
-		var poo = CreateCharacter(Player.PrefabPath, transform, "プー", "おれの名はプー。君達と共に戦う者だ。おれはネスに従う。", 100) as Player;
-		var starman = CreateCharacter(Enemy.PrefabPath, transform, "スターマン", "オマエハ 英雄デハナク タダノムシケラナノダ！", 1000) as Enemy;
-		var masterBelch = CreateCharacter(Enemy.PrefabPath, transform, "ゲップー", "ゲボゲボゲボ、ゲボゲボ、ゲローーップ！", 1000) as Enemy;
-		var mama = CreateCharacter(NPC.PrefabPath, transform, "ママ", "イエーイ！ファイト！ファ・イ・ト♪", 0) as NPC;
-		var mrSaturn = CreateCharacter(NPC.PrefabPath, transform, "どせいさん", "ぽえ〜ん", 0) as NPC;
+		var ness = CreateCharacter(Player.PrefabPath, transform, "ネス", "...", 5, 100) as Player;
+		var paula = CreateCharacter(Player.PrefabPath, transform, "ポーラ", "…わたしもちょっとくらいならアブナイ超能力を使えるのよ。", 2, 60) as Player;
+		var jeff = CreateCharacter(Player.PrefabPath, transform, "ジェフ", "説明はいらないよ。ぼくはジェフ。きみたちに呼ばれて来たんだ。", 3, 70) as Player;
+		var poo = CreateCharacter(Player.PrefabPath, transform, "プー", "おれの名はプー。君達と共に戦う者だ。おれはネスに従う。", 7, 120) as Player;
+		var starman = CreateCharacter(Enemy.PrefabPath, transform, "スターマン", "オマエハ 英雄デハナク タダノムシケラナノダ！", 10, 200) as Enemy;
+		var masterBelch = CreateCharacter(Enemy.PrefabPath, transform, "ゲップー", "ゲボゲボゲボ、ゲボゲボ、ゲローーップ！", 20, 300) as Enemy;
+		var mama = CreateCharacter(NPC.PrefabPath, transform, "ママ", "イエーイ！ファイト！ファ・イ・ト♪", 0, 0) as NPC;
+		var mrSaturn = CreateCharacter(NPC.PrefabPath, transform, "どせいさん", "ぽえ〜ん", 0, 0) as NPC;
 	}
 }
