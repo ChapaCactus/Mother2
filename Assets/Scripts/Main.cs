93483782547c4b9741ad78b07d7fb021cfe7d007
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-13 18:21:08 +0900

    キャラマスターを作成&読み込み

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -11,24 +11,25 @@ public class Main : MonoBehaviour
 		StartGame();
 	}
 
-	private Character CreateCharacter(string prefabPath, Transform parent, string name, string talkMessage, int power, int hp)
+	private Character CreateCharacter(string prefabPath, Transform parent, string masterID)
 	{
+		var master = MasterLoader.LoadCharaMaster(masterID);
 		var prefab = Resources.Load(prefabPath) as GameObject;
 		var character = Instantiate(prefab, parent).GetComponent<Character>();
-		character.SetData(name, talkMessage, power, hp);
+		character.SetData(master.charaName, master.talkMessage, master.power, master.hp);
 
 		return character;
 	}
 
 	public void StartGame()
 	{
-		var ness = CreateCharacter(Player.PrefabPath, transform, "ネス", "...", 5, 100) as Player;
-		var paula = CreateCharacter(Friend.PrefabPath, transform, "ポーラ", "…わたしもちょっとくらいならアブナイ超能力を使えるのよ。", 2, 60) as Friend;
-		var jeff = CreateCharacter(Friend.PrefabPath, transform, "ジェフ", "説明はいらないよ。ぼくはジェフ。きみたちに呼ばれて来たんだ。", 3, 70) as Friend;
-		var poo = CreateCharacter(Friend.PrefabPath, transform, "プー", "おれの名はプー。君達と共に戦う者だ。おれはネスに従う。", 7, 120) as Friend;
-		//var starman = CreateCharacter(Enemy.PrefabPath, transform, "スターマン", "オマエハ 英雄デハナク タダノムシケラナノダ！", 10, 200) as Enemy;
-		//var masterBelch = CreateCharacter(Enemy.PrefabPath, transform, "ゲップー", "ゲボゲボゲボ、ゲボゲボ、ゲローーップ！", 20, 300) as Enemy;
-		var mama = CreateCharacter(NPC.PrefabPath, transform, "ママ", "イエーイ！ファイト！ファ・イ・ト♪", 0, 0) as NPC;
-		var mrSaturn = CreateCharacter(NPC.PrefabPath, transform, "どせいさん", "ぽえ〜ん", 0, 0) as NPC;
+		var ness = CreateCharacter(Player.PrefabPath, transform, "Ness") as Player;
+		var paula = CreateCharacter(Friend.PrefabPath, transform, "Paula") as Friend;
+		var jeff = CreateCharacter(Friend.PrefabPath, transform, "Jeff") as Friend;
+		var poo = CreateCharacter(Friend.PrefabPath, transform, "Poo") as Friend;
+		var starman = CreateCharacter(Enemy.PrefabPath, transform, "Starman") as Enemy;
+		var masterBelch = CreateCharacter(Enemy.PrefabPath, transform, "MasterBelch") as Enemy;
+		var mama = CreateCharacter(NPC.PrefabPath, transform, "Mama") as NPC;
+		var mrSaturn = CreateCharacter(NPC.PrefabPath, transform, "MrSaturn") as NPC;
 	}
 }
