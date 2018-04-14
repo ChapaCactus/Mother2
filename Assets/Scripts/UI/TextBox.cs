7262cabe987fe4c5982bf56c31c07502588b7034
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-14 12:41:43 +0900

    テキストボックス追加

---
+++ Assets/Scripts/UI/TextBox.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBox : MonoBehaviour
+{
+	[SerializeField]
+	private Text _text;
+
+	private float _showTimer;
+
+	private const float DefaultTimerSec = 5f;
+
+	public bool IsRunningTimer { get { return (_showTimer > 0); } }
+
+	private void Update()
+	{
+		if (!IsRunningTimer) return;
+
+		_showTimer -= Time.deltaTime;
+
+		if (_showTimer <= 0)
+		{
+			SetVisible(false);
+		}
+	}
+
+	public void SetVisible(bool visible)
+	{
+		gameObject.SetActive(visible);
+	}
+
+	public void SetText(string message)
+	{
+		if (_text == null) return;
+		if (string.IsNullOrEmpty(message)) return;
+
+		_text.text = message;
+		StartTimer();
+		SetVisible(true);
+	}
+
+	private void StartTimer()
+	{
+		_showTimer = DefaultTimerSec;
+	}
+}
