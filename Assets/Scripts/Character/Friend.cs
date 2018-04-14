039e458c628a254b5fe2ee42af2766c0566b4af5
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-15 03:55:26 +0900

    破棄時にDelegateの購読をやめる

--- Assets/Scripts/Character/Friend.cs
+++ Assets/Scripts/Character/Friend.cs
@@ -12,6 +12,11 @@ public class Friend : Character, IDamageable, ITalkable, IParty
 
 	public static readonly string PrefabPath = "Prefabs/Friend";
 
+	private void OnDisable()
+	{
+        CharacterManager.I.onPlayerMoved -= OnPlayerMoved;
+	}
+
 	public void Damage(int damage)
 	{
 		if (IsDead) return;
