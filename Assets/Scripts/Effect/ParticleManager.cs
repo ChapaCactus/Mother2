636c8f73263552518b307da6532f85dbe5fa789f
OganeMakiko <m.ogane@sky-link.co.jp>
2018-04-16 17:59:22 +0900

    ParticleManagerクラスにパーティクルを探すメソッドを実装

---
+++ Assets/Scripts/Effect/ParticleManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ParticleManager : SingletonMonoBehaviour<ParticleManager>
+{
+    [SerializeField]
+    private List<ParticleMaster> _masters = new List<ParticleMaster>();
+
+    public ParticleSystem SearchParticleSystem(string key)
+    {
+        if (_masters == null) return null;
+
+        return _masters.FirstOrDefault(master => master.key == key).particlePrefab;
+    }
+}
