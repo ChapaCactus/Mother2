38075c994ccaea5b89cd1f5f3be66084bd06a018
OganeMakiko <m.ogane@sky-link.co.jp>
2018-04-16 14:36:46 +0900

    エンカウントアニメーション追加 終了時のコールバックを設定

---
+++ Assets/Scripts/UI/Encount.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Encount : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+
+    private void Awake()
+    {
+        Assert.IsNotNull(_animator);
+        Play();
+    }
+
+    public void Play()
+    {
+        if (_animator == null) return;
+
+        _animator.Play("Encount");
+    }
+
+    public void OnEnd()
+    {
+        Debug.Log("エンカウントアニメ終了");
+    }
+}
