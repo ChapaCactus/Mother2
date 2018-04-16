8c47e18dce3d9a687c1ce468c9bc3b819a738ac8
OganeMakiko <m.ogane@sky-link.co.jp>
2018-04-16 17:56:13 +0900

    ParticleMasterクラス作成 Explodeマスター作成

---
+++ Assets/Scripts/Master/ParticleMaster.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ParticleMaster : ScriptableObject
+{
+    public string key = "";
+
+    public ParticleSystem particlePrefab;
+}
