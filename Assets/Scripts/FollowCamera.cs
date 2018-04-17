ec3b3cd1b46f1e52afe0a36cd768b8ce39cde725
Chapa.C(Takuya) <chapatown@gmail.com>
2018-04-17 17:27:29 +0900

    追従カメラ追加

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Player _player;
+
+    private const float ZPosition = -10;
+
+	private void Awake()
+	{
+        Assert.IsNotNull(_player);
+	}
+
+	private void Update()
+	{
+        if (_player == null) return;
+
+        var pos = new Vector3(_player.transform.localPosition.x, _player.transform.localPosition.y, ZPosition);
+        transform.localPosition = pos;
+	}
+}
