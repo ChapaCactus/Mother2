3f097c297d03d2e75dcf3f861907c726fc02f6c7
OganeMakiko <m.ogane@sky-link.co.jp>
2018-04-18 14:02:02 +0900

    Warpを作成

---
+++ Assets/Scripts/WarpPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WarpPoint : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 _nextScene_startPos = Vector2.zero;
+
+    [SerializeField]
+    private string _nextSceneName = "";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            SavedataManager.SaveLastPosition(_nextScene_startPos);
+            SavedataManager.SaveIsWarped(true);
+            SceneManager.LoadScene(_nextSceneName);
+            player.transform.localPosition = _nextScene_startPos;
+        }
+    }
+}
